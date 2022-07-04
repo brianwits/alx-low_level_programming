87292fa835c517a0f362374eb41cac7a3ef988a6
Brian Wito Mugendi <brianwits@gmail.com>
2022-07-04 21:11:21 +0300

    winner

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -17,10 +17,13 @@ n = rand() - RAND_MAX / 2;
 printf("Last digit of %i is %i and is ", n, (n % 10));
 
 if (n % 10) == 0;
+
 {
 printf("0\n");
 }
+
 else if (n % 10) > 5;
+
 {
 printf("greater than 5\n");
 }
