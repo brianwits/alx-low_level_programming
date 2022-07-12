2b0faf2813f038a232618d11e17173c7bbabb1ea
brianwits <brianwits@gmail.com>
2022-07-12 02:04:42 -0700

    ISUPPER

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _isupper - This function checkes for uppercase characters::wq::
+ *
+ * Return: Always 0.
+ */
+int _isupper(int c)
+
+{
+if (c >= 'A' && c <= 'z')
+{
+return (1);
+}
+
+else
+{
+return (0);
+}
+
+}
