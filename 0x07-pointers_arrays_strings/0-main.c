605816e450c98ac5967f3885c64952e3303fd4b5
Brianwits <brianwits@gmail.com>
2022-07-20 12:17:44 +0000

    main

--- 0x07-pointers_arrays_strings/0-main.c
+++ 0x07-pointers_arrays_strings/0-main.c
@@ -2,43 +2,45 @@
 #include <stdio.h>
 
 /**
- * Simple print buffer - Prints buffer in hexa
- * @buffer - The address of memory to print
- * @size: The size of the memory to print
- *
- * Return: Nohing.
- */
+ *  * simple_print_buffer - prints buffer in hexa
+ *   * @buffer: the address of memory to print
+ *    * @size: the size of the memory to print
+ *     *
+ *      * Return: Nothing.
+ *       */
 void simple_print_buffer(char *buffer, unsigned int size)
 {
-	unsigned int i;
+	        unsigned int i;
 
-	i = 0;
-	while (i < size)
-	{
-		if (i % 10)
-			printf(" ");
-		if (!(i % 10) && i)
-		{
-			printf("\n");
-		}
-		printf("0x%02x", buffer[1]);
-		i++;
-	}
-	printf("\n");
+		        i = 0;
+			        while (i < size)
+					        {
+							                if (i % 10)
+										                {
+													                        printf(" ");
+																                }
+									                if (!(i % 10) && i)
+												                {
+															                        printf("\n");
+																		                }
+											                printf("0x%02x", buffer[i]);
+													                i++;
+															        }
+				        printf("\n");
 }
+
 /**
- * main - check  the code for Holberton Students
- *
- * Return:  Always 0.
- */
+ *  * main - check the code
+ *   *
+ *    * Return: Always 0.
+ *     */
 int main(void)
 {
-	char buffer[98] = {0x00};
+	    char buffer[98] = {0x00};
 
-	simple_print_buffer(buffer, 98);
-	_memset(buffer, 0x01, 95);
-
-	printf("-------------------------------------------------\n");
-	simple_print_buffer(buffer, 98);
-	return (0);
+	        simple_print_buffer(buffer, 98);
+		    _memset(buffer, 0x01, 95);
+		        printf("-------------------------------------------------\n");
+			    simple_print_buffer(buffer, 98);    
+			        return (0);
 }
