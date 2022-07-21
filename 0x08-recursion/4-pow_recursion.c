74860f65f374113572cd2deb74e42d60535a7d97
Brianwits <brianwits@gmail.com>
2022-07-21 11:53:53 +0000

    X power Y

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -4,7 +4,7 @@
  * _pow_recursion - raise x to power y
  * @x: number
  * @y: power to raise to
- * Return :power of a number, -1 if power is less than 0
+ * Return: power of a number, -1 if power is less than 0
  */
 int _pow_recursion(int x, int y)
 {
